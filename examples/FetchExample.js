e6d9eb38b2aa67f28b3f72719e6833e457f28e7e
Patrick Stapfer <ryyppy@users.noreply.github.com>
2021-01-23 19:47:41 +0100

    Update FetchExample js output

--- examples/FetchExample.js
+++ examples/FetchExample.js
@@ -24,29 +24,29 @@ function login(email, password) {
     },
     body: JSON.stringify(body)
   };
-  return $$Promise.$$catch($$Promise.map($$Promise.$$then(globalThis.fetch("https://reqres.in/api/login", params), (function (res) {
-                        return res.json();
-                      })), (function (data) {
-                    var msg = data.error;
-                    if (!(msg == null)) {
-                      return {
-                              TAG: /* Error */1,
-                              _0: msg
-                            };
-                    }
-                    var token = data.token;
-                    if (token == null) {
-                      return {
-                              TAG: /* Error */1,
-                              _0: "Didn't return a token"
-                            };
-                    } else {
-                      return {
-                              TAG: /* Ok */0,
-                              _0: token
-                            };
-                    }
-                  })), (function (e) {
+  return $$Promise.$$catch(globalThis.fetch("https://reqres.in/api/login", params).then(function (res) {
+                    return res.json();
+                  }).then(function (data) {
+                  var msg = data.error;
+                  if (!(msg == null)) {
+                    return {
+                            TAG: /* Error */1,
+                            _0: msg
+                          };
+                  }
+                  var token = data.token;
+                  if (token == null) {
+                    return {
+                            TAG: /* Error */1,
+                            _0: "Didn't return a token"
+                          };
+                  } else {
+                    return {
+                            TAG: /* Ok */0,
+                            _0: token
+                          };
+                  }
+                }), (function (e) {
                 var msg;
                 if (e.RE_EXN_ID === $$Promise.JsError) {
                   var msg$1 = e._1.message;
@@ -69,16 +69,16 @@ function getProducts(token, param) {
   var params = {
     Authorization: "Bearer " + token
   };
-  return $$Promise.$$catch($$Promise.map($$Promise.$$then(globalThis.fetch("https://reqres.in/api/products", params), (function (res) {
-                        return res.json();
-                      })), (function (data) {
-                    var data$1 = data.data;
-                    var ret = (data$1 == null) ? [] : data$1;
-                    return {
-                            TAG: /* Ok */0,
-                            _0: ret
-                          };
-                  })), (function (e) {
+  return $$Promise.$$catch(globalThis.fetch("https://reqres.in/api/products", params).then(function (res) {
+                    return res.json();
+                  }).then(function (data) {
+                  var data$1 = data.data;
+                  var ret = (data$1 == null) ? [] : data$1;
+                  return {
+                          TAG: /* Ok */0,
+                          _0: ret
+                        };
+                }), (function (e) {
                 var msg;
                 if (e.RE_EXN_ID === $$Promise.JsError) {
                   var msg$1 = e._1.message;
@@ -99,26 +99,26 @@ var Product = {
 
 var FailedRequest = Caml_exceptions.create("FetchExample.FailedRequest");
 
-$$Promise.$$catch($$Promise.map($$Promise.$$then(login("emma.wong@reqres.in", "pw"), (function (ret) {
-                if (ret.TAG !== /* Ok */0) {
-                  return Promise.reject({
-                              RE_EXN_ID: FailedRequest,
-                              _1: "Login error - " + ret._0
-                            });
-                }
-                console.log("Login successful! Querying data...");
-                return getProducts(ret._0, undefined);
-              })), (function (result) {
-            if (result.TAG === /* Ok */0) {
-              console.log("\nAvailable Products:\n---");
-              return Belt_Array.forEach(result._0, (function (p) {
-                            console.log(String(p.id) + " - " + p.name);
-                            
-                          }));
+$$Promise.$$catch(login("emma.wong@reqres.in", "pw").then(function (ret) {
+            if (ret.TAG !== /* Ok */0) {
+              return Promise.reject({
+                          RE_EXN_ID: FailedRequest,
+                          _1: "Login error - " + ret._0
+                        });
             }
-            console.log("Could not query products: " + result._0);
-            
-          })), (function (e) {
+            console.log("Login successful! Querying data...");
+            return getProducts(ret._0, undefined);
+          }).then(function (result) {
+          if (result.TAG === /* Ok */0) {
+            console.log("\nAvailable Products:\n---");
+            return Belt_Array.forEach(result._0, (function (p) {
+                          console.log(String(p.id) + " - " + p.name);
+                          
+                        }));
+          }
+          console.log("Could not query products: " + result._0);
+          
+        }), (function (e) {
         if (e.RE_EXN_ID === FailedRequest) {
           console.log("Operation failed! " + e._1);
         } else {
